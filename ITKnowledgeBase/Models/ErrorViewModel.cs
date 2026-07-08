56c93a87f008b29f330735982e1218184c846499
user <user@DESKTOP-SRTBHQ5>
2026-07-08 19:28:57 +0300

    Add project files.

---
+++ ITKnowledgeBase/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ITKnowledgeBase.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
